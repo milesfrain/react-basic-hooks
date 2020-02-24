fa40d97301cd666afbf9849736eea72ab1f79e29
Miles Frain <miles.frain@colorado.edu>
2020-02-24 11:58:27 -0800

    Example of logging with button click

--- src/Example.purs
+++ src/Example.purs
@@ -3,23 +3,19 @@ module Example where
 import Prelude
 
 import Effect (Effect)
+import Effect.Console (log)
 import React.Basic.DOM as R
 import React.Basic.Events (handler_)
-import React.Basic.Hooks (ReactComponent, component, useState, (/\))
-import React.Basic.Hooks as React
+import React.Basic.Hooks (ReactComponent, component)
 
 mkExample :: Effect (ReactComponent {})
 mkExample = do
-  component "Example" \_ -> React.do
-    count /\ setCount <- useState 0
-
+  component "Button" \_ -> React.do
     pure $
       R.div_
-        [ R.p_ [ R.text $ "You clicked " <> show count <> " times" ]
-        , R.button
-          { onClick: handler_ (setCount \_ -> count + 1)
-          , children:
-            [ R.text "Click me" ]
+        [ R.button
+          { onClick: handler_ $ log "hello"
+          , children: [ R.text "Hello in the console" ]
           }
         ]
 
